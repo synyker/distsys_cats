54a1bedea2cfbeab7b18ad25b5ec82e04f0bf529
Jonne Airaksinen <jonne.airaksinen@cs.helsinki.fi>
2014-10-09 22:57:50 +0300

    mouse.sh

--- mouse.sh
+++ mouse.sh
@@ -6,14 +6,15 @@ echo "pid: $pid"
 port=$(cat "nc_port_number")
 echo "$port"
 
-var=`nc -l -q 0 $port`
+var=`nc -v -l -q 0 $port`
 
 while true; do
 
     if [ "$var" == "MEOW" ]
     then
-        
+	echo "I WAS CAUGHT" 
+	break
     else
-        var=`nc -l -q 0 $port`
+        var=`nc -v -l -q 0 $port`
     fi
 done
