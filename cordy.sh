4162465147e861000d7ef76af50bafbe10af4b9d
Jonne Airaksinen <jonne.airaksinen@gmail.com>
2014-10-09 17:23:34 +0300

    cordy reading nodes

--- cordy.sh
+++ cordy.sh
@@ -15,6 +15,14 @@
 # N ukkoXXX catname    No mouse on ukkoXXX,
 # G ukkoXXX catname    Got the mouse
 
+
+linecounter=1
+node=$(sed -n "$linecounterp" < ukkonodes)
+echo $node
+linecounter=$[$linecounter+1]
+node=$(sed -n "$linecounterp" < ukkonodes)
+echo $node
+
 while true; do
 
 	count=$(cat cmsg | wc -l)
@@ -22,6 +30,24 @@ while true; do
 	if [ $count -gt 0 ]
 	then
 		line=$(head -n 1 cmsg)
+
+		# Found the mouse, send the other cat to the same node
+		if [ ${line:0:1} == "F"]
+		then
+			node=${line:2:7}
+			echo "$node"
+
+		# No mouse at the node, send the cat to the next node
+		elif [ ${line:0:1} == "N"]
+		then
+
+
+		# The mouse was caught
+		elif [ ${line:0:1} == "G"]
+		then
+		
+		fi
+
 		sed -i 1d cmsg
 
 		echo "$line"
