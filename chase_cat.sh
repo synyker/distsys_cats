88bebdecd30f536e6653859981b2f18f1a4923fd
Jonne Airaksinen <jonne.airaksinen@gmail.com>
2014-10-09 21:32:06 +0300

    chase_chat.sh

--- chase_cat.sh
+++ chase_cat.sh
@@ -1,1 +1,23 @@
 # Catty and Jazzy communicate to Cordy through Listy cat.
+
+command=$1
+name=$2
+port=$(cat $file"/nc_port_number")
+
+if [ $command == "S" ]
+then
+	echo "SEARCHING"
+	res=$(netcat -v -w 0 localhost $port)
+	if [ ${res:-10} == "succeeded!" ]
+	then
+		echo "success!"
+	fi
+
+elif [ $command == 'A' ]
+then
+	
+	
+	
+
+	curhost=$(hostname)
+fi
